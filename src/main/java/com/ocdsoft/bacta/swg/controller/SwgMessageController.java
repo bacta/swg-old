e267ff7828eba4dc9dc398a78cd442dccf8a624c
Kyle Burkhardt <kburkhardt@utah.gov>
2015-01-27 19:36:03 -0700

    [Update] Swg Routing

--- src/main/java/com/ocdsoft/bacta/swg/controller/SwgMessageController.java
+++
@@ -1,8 +0,0 @@
-package com.ocdsoft.bacta.swg.controller;
-
-import com.ocdsoft.bacta.soe.connection.SoeUdpConnection;
-import com.ocdsoft.bacta.soe.message.GameNetworkMessage;
-
-public interface SwgMessageController<Client extends SoeUdpConnection, Data extends GameNetworkMessage>  {
-    void handleIncoming(Client client, Data message) throws Exception;
-}
