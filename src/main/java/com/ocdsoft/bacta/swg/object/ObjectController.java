baac37dcdfb9aca27690780b86a058c488d1e78d
Kyle Burkhardt <mustangcoupe69@gmail.com>
2015-02-06 16:32:41 -0700

    [Update] ObjControllers processing

--- src/main/java/com/ocdsoft/bacta/swg/object/ObjectController.java
+++ src/main/java/com/ocdsoft/bacta/swg/object/ObjectController.java
@@ -1,7 +1,9 @@
 package com.ocdsoft.bacta.swg.object;
 
+import com.ocdsoft.bacta.engine.buffer.ByteBufferWritable;
+
 /**
  * Created by kburkhardt on 2/6/15.
  */
-public interface ObjectController {
+public interface ObjectController extends ByteBufferWritable{
 }
