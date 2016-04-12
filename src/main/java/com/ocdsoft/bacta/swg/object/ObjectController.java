e898591a18a6a54dcefc619232d490363d71fbde
Kyle Burkhardt <mustangcoupe69@gmail.com>
2016-04-11 22:59:00 -0600

    [Update] Moved some code

--- src/main/java/com/ocdsoft/bacta/swg/object/ObjectController.java
+++
@@ -1,9 +0,0 @@
-package com.ocdsoft.bacta.swg.object;
-
-import com.ocdsoft.bacta.engine.buffer.ByteBufferWritable;
-
-/**
- * Created by kburkhardt on 2/6/15.
- */
-public interface ObjectController extends ByteBufferWritable{
-}
